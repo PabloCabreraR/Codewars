fe993ba2006a16317d4b8f20cf9f75ed5650998c
PabloCabreraR <pablocabrera2g@gmail.com>
2021-04-14 10:52:42 +0200

    Coding exercises from Codewars

---
+++ 4kyu/stripComments.js
@@ -0,0 +1,31 @@
+// INSTRUCTIONS
+
+// Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+// Example:
+// Given an input string of:
+// apples, pears # and bananas
+// grapes
+// bananas !apples
+// The output expected would be:
+// apples, pears
+// grapes
+// bananas
+// The code would be called like so:
+// var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+// // result should == "apples, pears\ngrapes\nbananas"
+
+
+// SOLUTION
+function solution(input, markers) {
+  
+    input = input.split(`\n`)
+    
+    for (let i = 0; i < markers.length; i++) {
+      
+      let reg = new RegExp(`\\${markers[i]}.+`, "g")
+      
+      input = input.map(element => element.replace(reg, "").trim())
+      
+    }
+    return input.join(`\n`)
+}
