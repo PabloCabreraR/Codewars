6c352ca9f7e50fb2988a83a89841e5db8e5bea33
PabloCabreraR <pablocabrera2g@gmail.com>
2021-04-15 17:59:43 +0200

    Coding exercises from Codewars

---
+++ 8kyu/lostWithoutAMap.js
@@ -0,0 +1,9 @@
+// INSTRUCTIONS
+
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+
+
+// SOLUTION
+const maps = (x) => x.map(a=>a*2)
