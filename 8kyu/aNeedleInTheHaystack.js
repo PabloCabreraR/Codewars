14e85bbf592c9fa25abb315a025c9ffe25fe69a3
PabloCabreraR <pablocabrera2g@gmail.com>
2021-04-15 17:55:31 +0200

    Coding exercises from Codewars

---
+++ 8kyu/aNeedleInTheHaystack.js
@@ -0,0 +1,20 @@
+// ISNTRUCTIONS
+
+// Can you find the needle in the haystack?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+// should return "found the needle at position 5"
+
+
+// SOLUTION
+const findNeedle = (arr) => {
+    let result
+    arr.forEach((el, i)=>{
+      if (el === 'needle'){
+        result = i
+      }
+    })
+    return `found the needle at position ${result}`
+  }
