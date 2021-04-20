4f2315513c1a6c94cd1c7709e1ad5837644b47fa
PabloCabreraR <pablocabrera2g@gmail.com>
2021-04-20 10:08:40 +0200

    Coding exercises from Codewars

---
+++ 8kyu/powersOfTwo.js
@@ -0,0 +1,17 @@
+// INSTRUCTIONS
+
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive).
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+
+// SOLUTION
+const powersOfTwo = (n) => {
+    let arr = []
+    for (let i = 0; i<=n ; i++){
+      arr.push(2**i)
+    }
+    return arr
+  }
