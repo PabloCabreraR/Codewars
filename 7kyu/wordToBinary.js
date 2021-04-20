befde0dc0168d951b9c24112757c320a5177c909
PabloCabreraR <pablocabrera2g@gmail.com>
2021-04-20 09:34:30 +0200

    Lab done

---
+++ 7kyu/wordToBinary.js
@@ -0,0 +1,10 @@
+// INSTRUCTIONS
+
+// Write a function that takes a string and returns an array containing binary numbers equivalent to the ASCII codes of the characters of the string. The binary strings should be eight digits long.
+// Example: 'man' should return [ '01101101', '01100001', '01101110' ]
+
+
+// SOLUTION
+const wordToBin = (str) => {
+    return str.split('').map(el=>'0'+el.charCodeAt(0).toString(2))
+}
