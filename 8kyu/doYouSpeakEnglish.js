8fe687cefb82320ee8c008a919c2912af5218a99
PabloCabreraR <pablocabrera2g@gmail.com>
2021-04-29 17:21:57 +0200

    Coding exercises from Codewars

---
+++ 8kyu/doYouSpeakEnglish.js
@@ -0,0 +1,13 @@
+// INSTRUCTIONS
+
+// Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+// The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+// Return value as boolean values, true for the string to contains "English", false for it does not.
+
+
+// SOLUTION
+const spEng = (sentence) => {
+    const regex = /english/ig
+    return sentence.match(regex) ? true : false
+}
