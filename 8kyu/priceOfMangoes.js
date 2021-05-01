190da06e605541ccd75c0e9683690c79aacd3a35
PabloCabreraR <pablocabrera2g@gmail.com>
2021-05-01 23:29:43 +0200

    Coding exercises from Codewars

---
+++ 8kyu/priceOfMangoes.js
@@ -0,0 +1,13 @@
+// INSTRUCTIONS
+
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+// Examples
+// mango(3, 3) ==> 6    # 2 mangoes for 3 = 6; +1 mango for free
+// mango(9, 5) ==> 30   # 6 mangoes for 5 = 30; +3 mangoes for free
+
+
+// SOLUTION
+const mango = (quantity, price) => {
+  const operator=Math.floor(quantity/3)
+  return (quantity-operator)*price
+}
