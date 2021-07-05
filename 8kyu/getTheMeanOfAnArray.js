a4b1c6d4f642df6b0d80384c842c858554358797
PabloCabreraR <pablocabrera2g@gmail.com>
2021-07-05 09:21:13 +0200

    Coding exercises from Codewars

---
+++ 8kyu/getTheMeanOfAnArray.js
@@ -0,0 +1,9 @@
+// INSTRUCCTIONS
+
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// Return the average of the given array rounded down to its nearest integer.
+// The array will never be empty.
+
+
+// SOLUTION
+const getAverage = (marks) => Math.floor(marks.reduce((acc, el) => acc + el, 0)/marks.length)
