e98e61efeb6368d18c880a348933a57d10bfacee
PabloCabreraR <pablocabrera2g@gmail.com>
2021-10-15 15:37:55 +0200

    Coding exercises from Codewars

---
+++ Python/6kyu/sumOfDigits.py
@@ -0,0 +1,25 @@
+# INSTRUCCTIONS
+
+# Digital root is the recursive sum of all the digits in a number.
+# Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+# Examples
+#     16  -->  1 + 6 = 7
+#    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+# 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+# 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+
+# SOLUTION
+
+def digital_root(n):
+    n_to_string = str(n)
+    
+    while len(n_to_string) > 1:
+        sum = 0
+        for i in range(len(n_to_string)):
+            sum += int(n_to_string[i])
+            
+        n_to_string = str(sum)
+        
+    return int(n_to_string)
+        
