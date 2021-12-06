978a152d12f3df4ebd892c249a596e904e13ed3d
PabloCabreraR <pablocabrera2g@gmail.com>
2021-12-06 15:13:05 +0100

    Coding exercises from Codewars

---
+++ Python/7kyu/getNames().py
@@ -0,0 +1,18 @@
+# INSTRUCTIONS
+
+# The following code is not giving the expected results. Can you figure out what the issue is?
+# The following is an example of data that would be passed in to the function.
+# data = [
+#     {'name': 'Joe', 'age': 20},
+#     {'name': 'Bill', 'age': 30},
+#     {'name': 'Kate', 'age': 23}
+# ]
+# get_names(data) # should return ['Joe', 'Bill', 'Kate']
+
+# SOLUTION
+
+def itemgetter(item):
+    return item['name']
+    
+def get_names(data):
+    return list(map(itemgetter, data))
